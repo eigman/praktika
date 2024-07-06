c41c731ea06ff16fecee6ac89080bf4089ce9cd1
Therapbit <wol.k@mail.ru>
2024-07-06 19:31:32 +0300

    ,mnhgfd
    nbvds

--- app/src/main/java/com/example/journal/MainActivity.kt
+++ app/src/main/java/com/example/journal/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.journal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.example.journal.databinding.ActivityMainBinding
@@ -17,17 +18,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val db = mainDb.getDb(this)
-        binding.button.setOnClickListener {
-            val group = group(
-                binding.enNumber.text.toString(),
-                binding.edGroup.text.toString()
-            )
-            Thread {
-                db.getDao().insertGroup(group)
-            }.start()
-
-
+        binding.group.setOnClickListener {
+            val intent = Intent(this, AddGroupActivity::class.java)
+            startActivity(intent)
         }
 
 
