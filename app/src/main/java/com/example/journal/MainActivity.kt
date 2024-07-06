681ac5508ea5360939979323d563dafe5a5799c7
Therapbit <wol.k@mail.ru>
2024-07-06 18:41:25 +0300

    Пусть будет так

--- app/src/main/java/com/example/journal/MainActivity.kt
+++ app/src/main/java/com/example/journal/MainActivity.kt
@@ -2,46 +2,36 @@ package com.example.journal
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.journal.ui.theme.JournalTheme
+import com.example.journal.databinding.ActivityMainBinding
+
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            JournalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val db = mainDb.getDb(this)
+        binding.button.setOnClickListener {
+            val group = group(
+                binding.enNumber.text.toString(),
+                binding.edGroup.text.toString()
+            )
+            Thread {
+                db.getDao().insertGroup(group)
+            }.start()
+
+
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JournalTheme {
-        Greeting("Android")
     }
+
+
 }
